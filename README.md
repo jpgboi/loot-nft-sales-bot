597923db60588046ab7438b5dbdc333aca6e16ff
jpgboi <jpgboi@protonmail.com>
2021-08-29 17:33:06 +0200

    log transfer events and their value

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sales Bot
+
+- `abi.json` is from the [Loot Poject](https://etherscan.io/address/0xff9c1b15b16263c61d017ee9f65c50e4ae0113d7) but should work with any ERC-721 compatible contract
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hi')
+import dotenv from 'dotenv'
+import { BigNumber, Contract, Event, providers } from 'ethers'
+import abi from './abi.json'
+
+dotenv.config()
+
+const rpc = new providers.JsonRpcProvider(process.env.PROVIDER_URL)
+const contract = new Contract(process.env.CONTRACT_ADDRESS, abi, rpc)
+
+async function main() {
+  console.log('listening')
+
+  contract.on(
+    'Transfer',
+    async (from: string, to: string, tokenId: BigNumber, event: Event) => {
+      const { value } = await event.getTransaction()
+      console.log({ from, to, tokenId, value })
+    },
+  )
+}
+
+main()
