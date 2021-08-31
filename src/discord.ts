6fc3e1d0e0a731979aff87b6ccedd18aa650f037
Pierre Bertet <hello@pierre.world>
2021-08-31 01:16:22 +0100

    Display items list with rarity

--- src/discord.ts
+++ src/discord.ts
@@ -1,4 +1,7 @@
-import { WebhookClient } from 'discord.js'
+import { MessageAttachment, WebhookClient } from 'discord.js'
+import { imageRarityFromItems } from 'loot-rarity'
+import svg2img from 'svg2img'
+
 import { Message } from './types'
 
 const url = process.env.DISCORD_WEBHOOK_URL
@@ -9,10 +12,54 @@ if (!url) {
 
 const webhookClient = new WebhookClient({ url })
 
-export const sendDiscordMessage = async ({ tokenId, eth, usd }: Message) => {
-  webhookClient
-    .send(`Bag #${tokenId}\nBought for ${eth} ETH ($${usd})`)
-    .catch((error) => {
-      console.error('Error while sending Discord message', error)
+function shortenAddress(address: string) {
+  return address.slice(0, 6) + '…' + address.slice(-4)
+}
+
+// Discord doesn’t support data URI nor SVG files,
+// so we need to convert the data URI SVG into a PNG buffer.
+async function rarityImageBuffer(items: string[]): Promise<Buffer> {
+  const dataUriImage = imageRarityFromItems(items)
+  const svg = decodeURIComponent(dataUriImage).split(',')[1]
+  return new Promise((resolve, reject) => {
+    svg2img(svg, { width: 350, height: 350 }, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
     })
+  })
+}
+
+export const sendDiscordMessage = async ({
+  eth,
+  from,
+  loot,
+  to,
+  tokenId,
+  usd,
+}: Message) => {
+  const attachment = new MessageAttachment(
+    await rarityImageBuffer(Object.values(loot)),
+    'loot.png'
+  )
+
+  const message = {
+    username: 'Loot Sales',
+    embeds: [
+      {
+        title: `Bag #${tokenId}`,
+        url: `https://opensea.io/assets/${process.env.CONTRACT_ADDRESS}/${tokenId}`,
+        color: 0x000000,
+        description: `**Bought for ${eth} ETH ($${usd})**`,
+        image: { url: 'attachment://loot.png' },
+        footer: {
+          text: `💰 by ${shortenAddress(to)} from ${shortenAddress(from)}`,
+        },
+      },
+    ],
+    files: [attachment],
+  }
+
+  webhookClient.send(message).catch((error) => {
+    console.error('Error while sending Discord message', error)
+  })
 }
